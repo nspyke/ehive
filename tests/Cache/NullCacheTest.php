375f824f608c997a62a105b8d59d7c7bce8856dd
Nik Spijkerman <nikspijkerman@gmail.com>
2017-08-29 12:50:09 +1200

    Licenses

--- tests/Cache/NullCacheTest.php
+++ tests/Cache/NullCacheTest.php
@@ -1,9 +1,8 @@
 <?php
-
-/*
- * This file is part of the Symfony package.
+/**
+ * This file is part of the nspyke/ehive library.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * Copyright (c) 2017. Nik Spijkerman.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- tests/Transport/TransportTest.php
+++ tests/Transport/TransportTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the nspyke/ehive library.
+ *
+ * Copyright (c) 2017. Nik Spijkerman.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace EHive\Tests\Transport;
 
